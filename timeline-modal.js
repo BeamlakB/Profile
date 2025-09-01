52c002d3965e9e71afb52235a3b4362aa534c22f
BeamlakB <bbekele1@umbc.edu>
2025-09-01 00:29:55 -0400

    fixed links and spelling

--- timeline-modal.js
+++ timeline-modal.js
@@ -6,18 +6,18 @@ function initTimelineModals() {
       text: "During my 11-week HPC internship, I began with a 3-week boot camp where my team and I built a high-performance computing cluster from the ground up. We booted drives, converted nodes into stateless systems, enabled network booting, and implemented OpenChami for cluster management. We deployed a Slurm scheduler for job submission and used Ansible for configuration automation and CI/CD pipelines. Building on this foundation, we developed a containerized GPU-accelerated molecular dynamics workflow using GROMACS with CUDA-aware MPI across two NVIDIA GPU nodes. With Open OnDemand (OOD), we streamlined deployment and visualization—transforming what was once a 30-minute command-line process into a 5-minute web interface interaction, significantly improving efficiency and usability. \n To further enhance performance, I integrated Grafana dashboards with Prometheus to monitor real-time system resource usage, replacing tedious manual checks with intuitive visualizations that supported smarter workflow optimization. I also applied Ruby for UI customizations and worked extensively with Linux system services, gaining experience with web certificates, monitoring tools, and HPC system interactions. By the end of the internship, our team successfully demonstrated the GPU-accelerated workflow and monitoring system, and we submitted our work at SC25, one of the leading HPC conferences. This experience strengthened my skills in cluster management, workflow automation, containerization, GPU computing, and monitoring tools, while also sharpening my teamwork, problem-solving, and technical presentation abilities." ,
       images:"",
       links: {
-        Paper: "/page/GPUnicorn_Paper.pdf",
-        poster: "/page/GPUnicorn_Final_Poster.pdf",
-        Abstract:"/page/GPUnicorn_Abstract.pdf"
+        Paper: "https://BeamlakB.github.io/Profile/page/GPUnicorn_Paper.pdf",
+        Poster: "https://BeamlakB.github.io/Profile/page/GPUnicorn_Final_Poster.pdf",
+        Abstract:"https://BeamlakB.github.io/Profile/page/GPUnicorn_Abstract.pdf"
       }
     },
     {
     title: "Computer Vison Engineer @ Vectech",
-    text: "During my internship at Vectech I focused on improving fine-grained mosquito species classification. In previous approaches, Top-K embeddings were randomly initialized and updated during training, which often led to misclassification among similar species. I proposed using static embeddings that incorporate taxonomy knowledge of the species to improve model performance. I generated embeddings using species names and taxonomy levels, reshaped via Gaussian Random Projection, and experimented with two approaches: one where embeddings served as initial weights for an LSTM, and another where embeddings remained constant during training. The dataset contained 32 classes, and I used PyTorch, LSTMs, and static embedding classes to integrate domain knowledge into the model. \n \n Integrating these embeddings significantly improved classification accuracy. Visualization with TSNE and UMAP showed that species were correctly clustered according to their genus and subgenus, with Silhouette Scores increasing from 0.52 to 0.97 and subgenus classification reaching 96% accuracy. Overall model accuracy improved across train, validation, and test sets, with the second static embedding method outperforming the baseline. Specific species like Ae_canadensis and Ae_tormentor increased in accuracy from ~62–66% to 83%. This work taught me advanced embedding techniques, visualization methods for analyzing embeddings, and practical applications of domain knowledge in machine learning, strengthening my skills in fine-grained classification, model evaluation, and Python-based ML workflows.",
-    image: "img/TSNE-vec.png",
+    text: "During my internship at Vectech I focused on improving fine-grained mosquito species classification. In previous approaches, Top-K embeddings were randomly initialized and updated during training, which often led to misclassification among similar species. I proposed using static embeddings that incorporate taxonomy knowledge of the species to improve model performance. I generated embeddings using species names and taxonomy levels, reshaped via Gaussian Random Projection, and experimented with two approaches: one where embeddings served as initial weights for an LSTM, and another where embeddings remained constant during training. The dataset contained 32 classes, and I used PyTorch, LSTMs, and static embedding classes to integrate domain knowledge into the model. \n \n Integrating these embeddings significantly improved classification accuracy. Visualization with TSNE and UMAP showed that species were correctly clustered according to their genus and subgenus, with Silhouette Scores increasing from 0.52 to 0.97 and subgenus classification reaching 96% accuracy. Overall model accuracy improved across train, validation, and test sets, with the second static embedding method outperforming the baseline. Specific species like Aedes Canadensis and Aedes Tormentor increased in accuracy from ~62–66% to 83%. This work taught me advanced embedding techniques, visualization methods for analyzing embeddings, and practical applications of domain knowledge in machine learning, strengthening my skills in fine-grained classification, model evaluation, and Python-based ML workflows.",
+    image: "https://BeamlakB.github.io/Profile/img/TSNE-vec.png",
     links: {
-        Experiment_report: "/page/Expermintal_report.pdf",
-        Visualization: "/page/TSNE_UMPA_220.pdf"
+        Experiment_report: "https://BeamlakB.github.io/Profile/page/Expermintal_report.pdf",
+        Visualization: "https://BeamlakB.github.io/Profile/page/TSNE_UMPA_220.pdf"
       
     }
    },
@@ -31,9 +31,9 @@ function initTimelineModals() {
    {
      title:"AI Research @ UMBC:Interactive Robotics and Language Lab",
     text:"As a researcher at UMBC’s Interactive Robotics and Language Lab, I investigated how large language models (LLMs) such as GPT-4, LLAMA-7b, and Mistral-7b interpret multimodal narratives combining text and images. Our methodology involved generating alternative story annotations by systematically altering key conditions and events within the narratives, ensuring coherence and realism across six components: goal, visually grounded entity, condition, alternate condition, alternate event, and rewritten story. I implemented pipelines to process human survey responses and model outputs, and evaluated annotations based on grounding, visual relevance, causal reasoning, and story coherence. This work was conducted on ARL, COIN, and V2C datasets, with GPT-4 handling multimodal inputs while LLAMA and Mistral processed textual inputs. Through this research, I developed expertise in multimodal NLP, LLM evaluation, structured annotation methods, and data pipeline development.",
-    image:"/img/IRL_image.png",
+    image:"https://BeamlakB.github.io/Profile/img/IRL_image.png",
     links:{
-        Report: "/page/Analysis.pdf",
+        Report: "https://BeamlakB.github.io/Profile/page/Analysis.pdf",
     }
    }
     
